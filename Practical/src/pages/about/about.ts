a2a8c2b0a932df77c2b7f2ed2782927e681fd190
chengchenghappy <1306073684@qq.com>
2018-12-03 14:44:07 +0800

    分享、消息1.1

--- Practical/src/pages/about/about.ts
+++ Practical/src/pages/about/about.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { VideoPage } from '../video/video';
+import { ArticlePage } from '../article/article';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
+  isActive=0;
+  isClick(i){
+    this.isActive=i;
+  }
+  arr=["视频","文章","关注"];
   constructor(public navCtrl: NavController) {
 
   }
-
+  goVideo(){
+    this.navCtrl.push(VideoPage);
+  }
+  goArticle(){
+    this.navCtrl.push(ArticlePage);
+  }
 }
--- Practical/src/pages/contact/contact.ts
+++ Practical/src/pages/contact/contact.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MyPage } from '../my/my';
+import { CommentPage } from '../comment/comment';
+import { GoodPage } from '../good/good';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  isActive=0;
+isClick(i){
+  this.isActive=i;
+}
+arr=["消息","通知"];
   constructor(public navCtrl: NavController) {
 
   }
-
+  goMy(){
+    this.navCtrl.push(MyPage);
+  }
+  goComment(){
+    this.navCtrl.push(CommentPage);
+  }
+  goGood(){
+    this.navCtrl.push(GoodPage);
+  }
 }
