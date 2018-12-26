96506d544a0fc00bbe96dcdb0e69115dd9353902
wangqingshun <2184801857@qq.com>
2018-12-26 17:24:02 +0800

    我的关注

--- 1-项目启动/食途项目界面原型/食途-前端原型设计/Practical/src/pages/collection/collection.ts
+++ 1-项目启动/食途项目界面原型/食途-前端原型设计/Practical/src/pages/collection/collection.ts
@@ -1,6 +1,6 @@
 import { Component, } from '@angular/core';
 import { IonicPage, NavController, NavParams, } from 'ionic-angular';
-
+import { Http } from '@angular/http'; 
 /**
  * Generated class for the CollectionPage page.
  *
@@ -16,8 +16,13 @@ import { IonicPage, NavController, NavParams, } from 'ionic-angular';
 })
 
 export class CollectionPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  arr;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http:Http) {
+    var id=window.localStorage.getItem('id');
+      this.http.get('/api/share/'+id).subscribe((data)=>{
+        this.arr=JSON.parse(data['_body']);
+        console.log(this.arr)
+       })
   }
   
 
