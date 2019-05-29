1d6f99b5cef5ec5d7c4adcf7094a331dbf64b286
zhangchengcheng123 <1306073684@qq.com>
2019-05-29 11:39:33 +0800

    search

--- 1-项目启动/食途项目界面原型/食途-后台原型设计/myserve/my_serve/routes/search.js
+++ 1-项目启动/食途项目界面原型/食途-后台原型设计/myserve/my_serve/routes/search.js
@@ -2,33 +2,88 @@ var express=require('express');
 var Usesql = require('../modules/usesql');
 var db = require('../modules/database');
 var router=express.Router();
+var fs   = require('fs');
+var qs   = require('querystring');
+var bodyParser = require('../node_modules/body-parser');
+var formidable = require('../node_modules/formidable');
 
-router.post('/',function(req,res){
+router.post('/:neirong',function(req,res){
+	res.append('Access-Control-Allow-Origin', "*");
+  	res.header('Access-Control-Allow-Origin', '*');
+  	res.setHeader("Access-Control-Allow-Origin", "*");
+  	var neirong=req.params['neirong'];
+	var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
 	db.getConnection(function(err,connection){
-      console.log(req.body);
-      	var name=req.body['name'];
-      	connection.query('SELECT name from z_xiangqing WHERE name=?',[name],function(err,res){
-        	if(!err){
+      	connection.query('SELECT neirong from z_xiangqing' ,(err,result)=>{
+        	if(!err){//存在相应的内容
             	var data={code:200,msg:"查询成功"};
-              res.json(data);
+              	res.json(data);
             }else{
               var data={code:1,err:err,msg:"没有相关内容"};
               res.json(err)
             };
+            connection.release();
         })
-      connection.release();
-    })
-})
-router.get('/',function(req,res){
-	db.getConnection(function(err,connection){
-      var phone =req.query['name'];
-    	connection.query('SELECT name from z_xiangqing WHERE name=?',[name],function(err,result){
-        	if(!err){
-            	res.json(result);
-            }
-        })
-      connection.release();
-    })
 })
 
-module.exports = router;
+
+module.exports = router;var express=require('express');
+var Usesql = require('../modules/usesql');
+var db = require('../modules/database');
+var router=express.Router();
+var fs   = require('fs');
+var qs   = require('querystring');
+var bodyParser = require('../node_modules/body-parser');
+var formidable = require('../node_modules/formidable');
+
+router.post('/:neirong',function(req,res){
+	res.append('Access-Control-Allow-Origin', "*");
+  	res.header('Access-Control-Allow-Origin', '*');
+  	res.setHeader("Access-Control-Allow-Origin", "*");
+  	var neirong=req.params['neirong'];
+	var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+	db.getConnection(function(err,connection){
+      	connection.query('SELECT neirong from z_xiangqing' ,(err,result)=>{
+        	if(!err){//存在相应的内容
+            	var data={code:200,msg:"查询成功"};
+              	res.json(data);
+            }else{
+              var data={code:1,err:err,msg:"没有相关内容"};
+              res.json(err)
+            };
+            connection.release();
+        })
+})
+module.exports = router;var express=require('express');
+var Usesql = require('../modules/usesql');
+var db = require('../modules/database');
+var router=express.Router();
+var fs   = require('fs');
+var qs   = require('querystring');
+var bodyParser = require('../node_modules/body-parser');
+var formidable = require('../node_modules/formidable');
+
+router.post('/:neirong',function(req,res){
+	res.append('Access-Control-Allow-Origin', "*");
+  	res.header('Access-Control-Allow-Origin', '*');
+  	res.setHeader("Access-Control-Allow-Origin", "*");
+  	var neirong=req.params['neirong'];
+	var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+	db.getConnection(function(err,connection){
+      	connection.query('SELECT neirong from z_xiangqing' ,(err,result)=>{
+        	if(!err){//存在相应的内容
+            	var data={code:200,msg:"查询成功"};
+              	res.json(data);
+            }else{
+              var data={code:1,err:err,msg:"没有相关内容"};
+              res.json(err)
+            };
+            connection.release();
+        })
+})
+
+
+module.exports = router;v
