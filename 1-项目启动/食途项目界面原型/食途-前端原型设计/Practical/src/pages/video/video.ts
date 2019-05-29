5eedc6a73f569a8e95687d4e9f35ea8db2a71206
guojiahao <1113234417@qq.com>
2019-05-29 10:19:40 +0800

    修改

--- 1-项目启动/食途项目界面原型/食途-前端原型设计/Practical/src/pages/video/video.ts
+++ 1-项目启动/食途项目界面原型/食途-前端原型设计/Practical/src/pages/video/video.ts
@@ -4,6 +4,12 @@ import { AuthorPage } from '../author/author';
 import { Http} from '@angular/http';
 import * as $ from 'jquery';
 
+/**
+ * Generated class for the VideoPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
 @IonicPage()
 @Component({
@@ -23,6 +29,7 @@ export class VideoPage {
   count=0;
   count1=123;
   start;
+  length;
   constructor(public http: Http,public navCtrl: NavController, public navParams: NavParams) {
     this.id=navParams.get('id');
     this.x_id=navParams.get('x_id');
@@ -36,6 +43,7 @@ export class VideoPage {
     this.http.get('/api/pinglun/'+this.x_id).subscribe((data)=>{
       this.pinglun=JSON.parse(data['_body']);
       console.log(this.pinglun);
+      this.length=JSON.parse(data['_body']).length;
     });
     var id=window.localStorage.getItem('id');
     this.http.get('/api/share/article/shoucang1/' + this.x_id, { params: { id:id } }).subscribe((data) => {
@@ -108,6 +116,7 @@ export class VideoPage {
       this.http.get('/api/pinglun/'+this.x_id).subscribe((data)=>{
         this.pinglun=JSON.parse(data['_body']);
         console.log(this.pinglun);
+        this.length=JSON.parse(data['_body']).length;
       })
     })
     this.EvaluateInfo.content='';
