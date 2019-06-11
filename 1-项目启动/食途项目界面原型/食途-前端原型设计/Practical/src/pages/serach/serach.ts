aa3bb9ac774fd37f51c7981582972d4ffaa726fe
zhangchengcheng123 <1306073684@qq.com>
2019-06-11 14:58:09 +0800

    页面优化

--- 1-项目启动/食途项目界面原型/食途-前端原型设计/Practical/src/pages/serach/serach.ts
+++ 1-项目启动/食途项目界面原型/食途-前端原型设计/Practical/src/pages/serach/serach.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SearchPage} from '../search/search';
 import { Http } from '@angular/http';
-
+// router.all('*', (ctx, next) => {  
+//   ctx.set('Access-Control-Allow-Origin', '*');  
+//   ctx.set('Access-Control-Allow-Credentials', true);  
+//   ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE');  
+//   ctx.set('Access-Control-Allow-Headers', 'x-requested-with, x-ui-request， lang');  
+//   next();  
+// });  
 
 @IonicPage()
 @Component({
@@ -10,6 +16,8 @@ import { Http } from '@angular/http';
   templateUrl: 'serach.html',
 })
 export class SerachPage {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http:Http) {
+  }
   txt:string;
   arr:string[]=[];
   menu=["鱼香肉丝",'毛血旺',"辣炒鱿鱼丝","宫爆鸡丁","辣子鸡","泡凤爪","土豆烧排骨","辣子鸡丁","灯影牛肉","干煸牛肉丝","冬菜扣肉",
@@ -19,6 +27,31 @@ export class SerachPage {
     "麻辣豆腐","蒜蓉蒸扇贝","鱼香肉丝－川菜","川北凉粉","香酥鸭","鱼香排骨","陈麻婆豆腐","羊肉火锅",'蒜香鲶鱼',
     '姜葱基围虾',"四川凉面","粉蒸牛肉","野生菌汤","辣子脆肠","脆皮肠头","泡菜炒肉末","麻辣牛肉干","蒜蓉蒸扇贝","川味猪肝",]
   search(){
+    var neirong=this.txt;
+    var obj={neirong:neirong};
+    var obj1=JSON.stringify(obj);
+    this.http.get('/api/search/4').subscribe((data)=>{
+      
+      console.log(data);
+    })
+   
+    // var oMyForm = new FormData();
+    // oMyForm.append("obj",obj1);
+    // var oReq = new XMLHttpRequest();
+    // oReq.onreadystatechange=function(){
+    //   if(oReq.readyState==4 && oReq.status==200){
+    //     var obj=JSON.parse(oReq.response);
+    //     if(obj.code==200){
+    //       Alert("查询成功");
+    //     }else{
+    //       Alert("没有相应内容")
+    //     }
+    //   }else{
+    //     console.log("查询失败")
+    //   }
+    // }
+    // oReq.open('POST','/api/search/'+neirong);
+    // oReq.send();
     if(this.txt==''){
       this.txt='重庆鸡公煲'
       this.arr.push(this.txt);
@@ -29,11 +62,6 @@ export class SerachPage {
     localStorage.setItem("history",JSON.stringify(this.arr));
     this.txt=""
     }
-    this.http.get('/api/search').subscribe((data)=>{
-
-    })
-  }
-  constructor(public navCtrl: NavController, public navParams: NavParams,public http:Http) {
   }
 
   ionViewDidLoad() {
