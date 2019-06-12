04a860298ac96b2cbe65d0968ca970dd8c7ad5e3
zhangchengcheng123 <1306073684@qq.com>
2019-06-12 18:35:04 +0800

    作品

--- 1-项目启动/食途项目界面原型/食途-前端原型设计/Practical/src/pages/works/works.ts
+++ 1-项目启动/食途项目界面原型/食途-前端原型设计/Practical/src/pages/works/works.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { ArticlePage } from '../article/article';
 
 /**
  * Generated class for the WorksPage page.
@@ -15,10 +17,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class WorksPage {
   public username:string;
-  ionViewWillEnter(){
-    this.username= window.localStorage.getItem('username');
+  arr=[];
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http:Http) {
+    var id=window.localStorage.getItem('id');
+    this.http.get('/api/share/').subscribe((data)=>{
+      var obj=JSON.parse(data['_body'])[1];
+      console.log(obj[1].id);
+      for(var i=0;i<obj.length-1;i++){
+        if(id==obj[i].id){
+          this.arr.push(obj[i])
+          console.log(obj[i]);
+        }
+      }
+    });
   }
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+  goArticle(x_id,id){
+    this.navCtrl.push(ArticlePage,{x_id:x_id,id:id});
+  }
+  ionViewWillEnter(){
   }
 
   ionViewDidLoad() {
