1ef07796f31c84afc5beb08f9c035d70d52dc388
zhangchengcheng123 <1306073684@qq.com>
2019-06-13 08:13:51 +0800

    消息

--- 1-项目启动/食途项目界面原型/食途-前端原型设计/Practical/src/pages/comment/comment.ts
+++ 1-项目启动/食途项目界面原型/食途-前端原型设计/Practical/src/pages/comment/comment.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { VideoPage } from '../video/video';
 import { ArticlePage } from '../article/article';
+import { Http } from '@angular/http';
 
 /**
  * Generated class for the CommentPage page.
@@ -16,8 +17,32 @@ import { ArticlePage } from '../article/article';
   templateUrl: 'comment.html',
 })
 export class CommentPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  arr=[];
+  array=[];
+  x_id;
+  pinglun;
+  length;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http:Http) {
+     var id=window.localStorage.getItem('id');
+    this.http.get('/api/pinglun/'+id).subscribe((data)=>{
+      this.pinglun=JSON.parse(data['_body']);
+      this.length=JSON.parse(data['_body']).length;
+      for(var i=0;i<this.length-1;i++){
+        if(this.pinglun[i].id==id){
+          this.array.push(this.pinglun[i].neirong);
+        }
+      }
+    });
+    this.http.get('/api/share/').subscribe((data)=>{
+      var obj=JSON.parse(data['_body'])[1];
+      console.log(obj[1].id);
+      for(var i=0;i<obj.length-1;i++){
+        if(id==obj[i].id){
+          this.arr.push(obj[i])
+          console.log(obj[i]);
+        }
+      }
+    });
   }
 
   ionViewDidLoad() {
